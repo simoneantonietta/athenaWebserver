af4783b86883e220623a53174c774f81f1a0e66a
Simone <simone.antonietta@saet.org>
2022-10-14 15:21:28 +0200

    Modificate pagine principali per essere più simili alla versione definitiva

--- webserver/compileServer.sh
+++ webserver/compileServer.sh
@@ -1,8 +1,8 @@
 make clean
 make
-sshpass -p "KMz0yCIq" ssh -t root@$1 "sed -i '$ d' /etc/inittab"
-sshpass -p "KMz0yCIq" ssh -t root@$1 "kill -HUP 1"
-sshpass -p "KMz0yCIq" ssh -t root@$1 "killall -9 server"
+#sshpass -p "KMz0yCIq" ssh -t root@$1 "sed -i '$ d' /etc/inittab"
+#sshpass -p "KMz0yCIq" ssh -t root@$1 "kill -HUP 1"
+#sshpass -p "KMz0yCIq" ssh -t root@$1 "killall -9 server"
 sshpass -p "KMz0yCIq" ssh -t root@$1 "rm -Rf /home/utente/serverroot/*"
 sshpass -p "KMz0yCIq" ssh -t root@$1 "rm -Rf /home/utente/serverfiles/*"
 sshpass -p "KMz0yCIq" scp server root@$1:/home/utente
